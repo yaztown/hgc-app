69a6301a0d6051a4a368a885b59759fbdc35479c
Yaztown <yaztown@gmail.com>
2019-03-12 16:10:08 +0300

    Created sensors component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hgc-app';
+  titleDeploy = 'Home Grow Center';
+  title = 'HGC';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SensorsComponent } from './sensors/sensors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorsComponent
   ],
   imports: [
     BrowserModule
