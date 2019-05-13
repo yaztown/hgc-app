e20279eebd80a13d35b48edaaaa935f0819c0b7c
Yaztown <yaztown@gmail.com>
2019-05-13 21:11:34 +0300

    using snake-case variable member names

--- src/app/controllers.ts
+++ src/app/controllers.ts
@@ -1,19 +1,25 @@
 import { Type } from '@angular/core';
 //
-export class DeviceController {
-  loopSleepTime: number;
+export class DeviceControllerJSON {
+  loop_sleep_time: number;
   name: string;
   daemon: boolean;
-  isAlive: boolean;
-  started: boolean;
-  exitLoop: boolean;
-  paused: boolean;
-  isStopped: boolean;
-  relayPin: number[];
-  manualControl: boolean;
-  deviceOn: boolean;
+  is_alive: boolean;
+  _started: boolean;
+  _exit_loop: boolean;
+  _paused: boolean;
+  _is_stopped: boolean;
+  relay_pin: number[];
+  manual_control: boolean;
+  _device_on: boolean;
   info: object;
 }
+
+export class DeviceController {
+  _type: string;
+  value: DeviceControllerJSON;
+}
+
 
 export class DeviceControllerItem {
   constructor(public component: Type<any>, _type: string, public data: any) {}
